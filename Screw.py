869f05b03bbb235f913d3fe6fedb50266ba9e313
Aun-kub <sittichai21146@gmail.com>
2025-09-19 22:51:19 +0700

    Update Screw.py

--- Screw.py
+++ Screw.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
 import pandas as pd
 
 # Define the calculation function (assuming this is the correct one based on previous interactions)
